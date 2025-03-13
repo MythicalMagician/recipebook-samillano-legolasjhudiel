55ca4d21628ee9707267b3e0c8d25b3c1b487765
MythicalMagician <legojhudiel@gmail.com>
2025-03-13 23:14:42 +0800

    Initial creation of recipebook/users and Profile class. Improved/adjusted templates.

---
+++ users/models.py
@@ -0,0 +1,12 @@
+# users/models.py
+""""""
+
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Profile(models.Model):
+    """"""
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    bio = models.TextField(blank=True, max_length=510)
